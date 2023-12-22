5357935a560a84981128e20a38c442931fd5ea1b
Jeremyugo <jeremyugoo@gmail.com>
2023-12-22 19:06:29 +0000

    fixed stable_diffusion model error

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
-from stable_diffusion import run_stable_diffusion
+import stable_diffusion
 
 # creating the app
 app = FastAPI()
@@ -12,7 +12,7 @@ def run_text2img(prompt: str, *,
     seed: int = 1000, num_inference_steps: int = 50, guidance_scale: float = 7.5):
     
     # runs the stable difussion function that generates the images
-    image = run_stable_diffusion(prompt, seed=seed, num_inference_steps=num_inference_steps,
+    image = stable_diffusion.run_stable_diffusion(prompt, seed=seed, num_inference_steps=num_inference_steps,
         guidance_scale=guidance_scale)
     
     image.save("text2img_output.png")
